9054722b4eb3d681e6a229a1d28f661c21883d5e
Martijn Sturm <martijn.sturm@gmail.com>
2020-09-16 16:49:32 +0200

    added generator for onlien groups

---
+++ utils/test_file.py
@@ -0,0 +1,24 @@
+from random import choices
+
+
+def generate_group_sequence(
+    n: int,
+    seq_range: range = range(1, 9),
+    probability: list = [0.2, 0.2, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05],
+):
+    if len(seq_range) != len(probability):
+        raise ValueError(
+            "number of items in range and probability list does not match.",
+            f"\nRange: {len(seq_range)}\n Prob: {len(probability)}",
+        )
+    sizes = list(seq_range)
+    return choices(sizes, probability, k=n)
+
+
+def write_test_file(filename, contents):
+    with open(f"input/{filename}.txt", mode="w") as file:
+        file.writelines([str(group) + "\n" for group in contents])
+
+
+write_test_file("new_test1", generate_group_sequence(100))
+
