96111c031e97ab5ceaf6fb8dca480ef93c07dbbd
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2020-09-24 13:59:36 +0200

    nieuwe input voor bnb

--- main_offline.py
+++ main_offline.py
@@ -1,6 +1,6 @@
 from problem.problem import Offline
 
 
-problem = Offline("input/test_input.txt")
+problem = Offline("input/offline_bnbChallenge.txt")
 problem.cinema.find_solution(problem.groups.freq_dict)
 # print(problem.cinema)
