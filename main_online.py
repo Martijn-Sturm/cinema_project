0685c38c6c18979c46346b6baf6abfd5f16b3bd0
ZorilOlah <ZorilOlah@gmail.com>
2020-09-25 14:27:25 +0200

    Update main_online.py

--- main_online.py
+++ main_online.py
@@ -1,4 +1,4 @@
-from algorithms.online2 import FirstFit, BestFit, WorstFit
+from algorithms.online2 import FirstFit, BestFit, WorstFit, MinCovidChairs
 
 first_fit = FirstFit("input/online_input_big1.txt")
 first_fit.execute("firstfit1")
@@ -11,3 +11,5 @@ best_fit.execute("bestfit1")
 worst_fit = WorstFit("input/online_input_big1.txt")
 worst_fit.execute("worstfit1")
 
+min_covid_chairs = MinCovidChairs("input/online_input_big1.txt")
+min_covid_chairs.execute("mincovidchairs1")
