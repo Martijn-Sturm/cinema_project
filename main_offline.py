d47e9ed2400cb953a8d9d6a0a79ea832aeecca2f
Esmee <eszclie@gmail.com>
2020-09-30 12:27:03 +0200

    Restructured offline_problem and fixed reading input files

--- main_offline.py
+++ main_offline.py
@@ -1,12 +1,4 @@
 from problem.problem import Offline
 
-problem = Offline("offline_challengingInput.txt")
-print(problem.cinema)
 
-neighboors_01 = problem.cinema.get_neighboors_from_coordinates((0, 1))
-
-print("\nNeighboors from (0,1)", neighboors_01)
-
-print("\nSame with coordinates:")
-for neighb in neighboors_01:
-    print(neighb, neighb.get_coordinates())
+Offline("input\\test_input.txt")
--- problem/problem.py
+++ problem/problem.py
@@ -1,16 +1,16 @@
 from .entities.input import Input
 from .entities.cinema import Cinema
-from .entities.groups import OfflineGroups, OnlineGroups
+from .entities.groups import OnlineGroups
+from problem.offline_problem import Problem
 
 
 class Offline:
     def __init__(self, filepath) -> None:
         # Read file
         file_input = Input(filepath, "offline")
-
-        # From file input, initialize cinema and groups object
-        self.cinema = Cinema(file_input.grid, file_input.row_nr, file_input.column_nr)
-        self.groups = OfflineGroups(file_input.groups)
+        p = Problem(file_input.grid, file_input.groups, file_input.row_nr, file_input.column_nr)
+        # p.print_grid()
+        p.get_solution()
 
 
 class Online:
