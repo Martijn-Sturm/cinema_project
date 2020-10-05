0a340f7404240689d7b4c6881626c1ee8852907d
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2020-10-05 11:34:46 +0200

    new input

--- main_offline.py
+++ main_offline.py
@@ -1,3 +1,3 @@
 from problem.problem import Offline
 
-Offline("/Users/clazinasteunenberg/Documents/GitHub/cinema_project/input/offline_mediumLargerInput.txt")
+Offline("offline_mediumLargestInput.txt")
