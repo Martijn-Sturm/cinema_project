3bdc9e6036be7e176f2ce3af6a07a782f6afa1bf
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2020-10-06 16:39:59 +0200

    new class for onfline

--- main_offline.py
+++ main_offline.py
@@ -1,9 +1,6 @@
 from problem.problem import Offline, Onfline
 import pickle
 
-# f = open("/Users/clazinasteunenberg/Documents/GitHub/cinema_project/output/onfline.txt", "wb")
-# dict = {}
 for i in range(21):
     name = "Exact" + str(i + 1)
-    Offline(name + ".txt")
-# pickle.dump(dict, f)
+    Offline("/Users/clazinasteunenberg/Documents/GitHub/cinema_project/input/" + name + ".txt")
