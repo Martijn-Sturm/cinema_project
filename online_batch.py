27a18fa5a53e1d13776b0d7018b9096c2912245f
Martijn Sturm <martijn.sturm@gmail.com>
2020-10-06 20:19:01 +0200

    add functions for online batch analysis

--- online_batch.py
+++ online_batch.py
@@ -1,10 +1,19 @@
 from utils.test_file import generate_group_sequence
 from algorithms.online import FirstFit, BestFit, WorstFit, MinCovidChairs, Hybrid_BF_CC
 import pandas as pd
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
+from os import listdir
 
 FILEPATH = "input/online_input_big1.txt"
-groups_list = [generate_group_sequence(50) for i in range(100)]
+GROUPS_LIST = [generate_group_sequence(50) for i in range(100)]
+FILE_DIR = "input/online"
+
+
+def run_algorithm_with_original_groups(algorithm, filepath):
+    alg = algorithm(filepath)
+    cinema_result = alg.execute()
+    return len(cinema_result.get_occupied_seats())
 
 
 def repeat_algorithm_with_different_groups(algorithm, filepath, groups_list):
@@ -18,29 +27,33 @@ def repeat_algorithm_with_different_groups(algorithm, filepath, groups_list):
     return results
 
 
-first_fit_result = repeat_algorithm_with_different_groups(
-    FirstFit, FILEPATH, groups_list
-)
-best_fit_result = repeat_algorithm_with_different_groups(BestFit, FILEPATH, groups_list)
-worst_fit_result = repeat_algorithm_with_different_groups(
-    WorstFit, FILEPATH, groups_list
-)
-min_covid_chairs_result = repeat_algorithm_with_different_groups(
-    MinCovidChairs, FILEPATH, groups_list
-)
-hybrid = repeat_algorithm_with_different_groups(Hybrid_BF_CC, FILEPATH, groups_list)
-df = pd.DataFrame(
-    {
-        "first": first_fit_result,
-        "best": best_fit_result,
-        "worst": worst_fit_result,
-        "minCovid": min_covid_chairs_result,
-        "hybrid": hybrid,
-    }
-)
+def get_file_names(directory):
+    return [file for file in listdir(directory)]
 
-df.describe()
-df.hist()
-plt.show()
-df.boxplot()
-plt.show()
+
+# first_fit_result = repeat_algorithm_with_different_groups(
+#     FirstFit, FILEPATH, GROUPS_LIST
+# )
+# best_fit_result = repeat_algorithm_with_different_groups(BestFit, FILEPATH, GROUPS_LIST)
+# worst_fit_result = repeat_algorithm_with_different_groups(
+#     WorstFit, FILEPATH, GROUPS_LIST
+# )
+# min_covid_chairs_result = repeat_algorithm_with_different_groups(
+#     MinCovidChairs, FILEPATH, GROUPS_LIST
+# )
+# hybrid = repeat_algorithm_with_different_groups(Hybrid_BF_CC, FILEPATH, GROUPS_LIST)
+# df = pd.DataFrame(
+#     {
+#         "first": first_fit_result,
+#         "best": best_fit_result,
+#         "worst": worst_fit_result,
+#         "minCovid": min_covid_chairs_result,
+#         "hybrid": hybrid,
+#     }
+# )
+
+# df.describe()
+# df.hist()
+# plt.show()
+# df.boxplot()
+# plt.show()
