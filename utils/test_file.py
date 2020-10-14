ef5318d22126d7942319d4b73910e09bf57d8b2f
Martijn Sturm <martijn.sturm@gmail.com>
2020-10-14 13:44:30 +0200

    add seed for sequence generation

--- utils/test_file.py
+++ utils/test_file.py
@@ -1,8 +1,9 @@
-from random import choices
+import random
 
 
 def generate_group_sequence(
     n: int,
+    seed,
     seq_range: range = range(1, 9),
     probability: list = [0.2, 0.2, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05],
 ):
@@ -12,7 +13,23 @@ def generate_group_sequence(
             f"\nRange: {len(seq_range)}\n Prob: {len(probability)}",
         )
     sizes = list(seq_range)
-    return choices(sizes, probability, k=n)
+    random.seed(seed)
+    return random.choices(sizes, probability, k=n)
+
+
+def generate_group_sequences(
+    n_groups: int,
+    seq_length: int,
+    seed,
+    seq_range: range = range(1, 9),
+    probability: list = [0.2, 0.2, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05],
+):
+    random.seed(seed)
+    seeds_list = [random.random() for i in range(n_groups)]
+    result = []
+    for new_seed in seeds_list:
+        result.append(generate_group_sequence(seq_length, new_seed))
+    return result
 
 
 def write_test_file(filename, contents):
@@ -20,5 +37,12 @@ def write_test_file(filename, contents):
         file.writelines([str(group) + "\n" for group in contents])
 
 
-# write_test_file("new_test1", generate_group_sequence(100))
+# result = generate_group_sequences(20, 10, "test")
+# print(result[0])
+# print(result[1])
+# print(result[2])
 
+# result = generate_group_sequences(20, 10, "test")
+# print(result[0])
+# print(result[1])
+# print(result[2])
